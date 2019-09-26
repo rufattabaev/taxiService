3ec47c44ee65ab73291f30b9a827532c20e3fb14
Руфат_Ольга <rufattabaev@gmail.com>
2019-09-26 19:30:32 +0300

    fixes #2

--- src/main/java/TaxiService.java
+++ src/main/java/TaxiService.java
@@ -1,13 +1,15 @@
 public class TaxiService {
+    int startOfDiscount = 1000;
+    int discountLimit = 100;
     int count(int boardingCost, int kilometerCost, int distance) {
         int totalCost = boardingCost + (kilometerCost * distance);
         int discount = (int) (totalCost - (totalCost * 0.95));
-        if (totalCost < 1000) {
+        if (totalCost < startOfDiscount) {
             totalCost = totalCost;
-        } else if (totalCost >= 1000 && discount <= 100) {
+        } else if (totalCost >= startOfDiscount && discount <= discountLimit) {
             totalCost = totalCost - discount;
         } else {
-            totalCost = totalCost - 100;
+            totalCost = totalCost - discountLimit;
         }
         return totalCost;
     }
